831724f1ad8226364fbd2a072084327333214e83
Matias Skjetne <matias.skjetne@nhn.no>
2025-04-09 10:52:34 +0200

    feat(nvim): added undodir

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,23 +1,27 @@
 -- general vim Settings
 
-vim.opt.encoding="utf-8"
-vim.opt.number=true
+vim.opt.encoding = "utf-8"
+vim.opt.number = true
 
 --- Indentation
-vim.opt.shiftwidth=4
-vim.opt.softtabstop=4
-vim.opt.tabstop=4
-vim.opt.expandtab=true
-vim.opt.smartindent=true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 --- Splitting
-vim.opt.splitbelow=true
-vim.opt.splitright=true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
-vim.opt.wrapscan=false
-vim.opt.ignorecase=true
-vim.opt.textwidth=80
-vim.opt.signcolumn="yes"
+vim.opt.wrapscan = false
+vim.opt.ignorecase = true
+vim.opt.textwidth = 80
+vim.opt.signcolumn = "yes"
 
 -- Scroll earlier
-vim.o.scrolloff = 8;
+vim.o.scrolloff = 8
+
+-- Use UndoDir
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+vim.opt.undofile = true
